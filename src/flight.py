a5b761ccd99655e52246c3b246ac046055df9878
Montserrat Regalado <montserratregalado@gmail.com>
2023-03-24 17:49:24 -0600

    changing to flight topic

---
+++ src/flight.py
@@ -0,0 +1,19 @@
+class Customer:
+    def __init__(self,name,email,phone):
+        self.name=name
+        self.email=email
+        self.phone=phone
+
+    def get_id(self):
+        return self.id
+    
+    def get_name(self):
+        return self.name
+    
+    def get_phone(self):
+        return self.phone
+    
+    def get_email(self):
+        return self.email
+
+    def set_name 
