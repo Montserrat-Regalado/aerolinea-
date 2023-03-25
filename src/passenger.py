8e1ef263983c36053efa645f088423f62bfce679
Montserrat Regalado <montserratregalado@gmail.com>
2023-03-25 10:00:57 -0600

    creating passenger file with methods

---
+++ src/passenger.py
@@ -0,0 +1,22 @@
+class Passenger():
+    def __init__(self, name,age,passportNumber):
+        self.name=name
+        self.age=age
+        self.passportNumber= passportNumber
+
+    def getPassengerDetails():
+        pass
+    
+    def makeReservation():
+        pass
+
+    def checkIn():
+        pass
+
+    def cancelReservation():
+        pass
+
+    def changeSeat():
+        pass
+
+    
